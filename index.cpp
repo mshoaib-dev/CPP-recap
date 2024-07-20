9ec36b51fc995c2104ba0005a144f071593c0060
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-20 03:16:30 -0700

    typeConversion

--- index.cpp
+++ index.cpp
@@ -4,34 +4,14 @@ using namespace std;
 int main()
 {
 /* 
-  arithmetic operators  = return the result of a specific arithmetic operation(+,-,*,/, %)
-  parenthesis
-  multiplication & division
-  addition & substration
+  type conversion  = conversion a value of one data type to another
+  Implicit = automatic i.e., truncating decimal value to whole no. using int data type.
+  Explicit = Precede value with new data type (int, string, char)
 */
-int stock = 100;
-
-//  +
-stock = stock + 1;
-stock+=1;
-stock++;
-
-//  -
-stock = stock - 1;
-stock-=1;
-stock--;
-//  *
-stock = stock * 2;
-stock*=2;
-
-//  /
-stock = stock / 20;
-stock/=20;
-
-// modulus: return the remainder
-
-int rem = stock % 20;
-  cout<<"Stock = "<<stock<<endl;
-  cout<<"Remainder = "<<rem;
+  char letter = 84;
+  int runs = 280;
+  int overs= 50;
+  double runRate = (double) runs/overs; 
+  cout<<letter<<"he run rate = "<< runRate; 
   return 0;
 };
