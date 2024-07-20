e9294ef53752a5dd2cda2e56c661161cc778443c
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-20 08:36:43 -0700

    calculator using switchCases

--- index.cpp
+++ index.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-/* 
-for math function: use cmath header file
-cplusplus.com/reference/math web resource
-*/
-  double base, perp, hypo;
-  
-  cout<<"Enter base value = ";
-  cin>>base;
-  cout<<"Enter perpendicular value = ";
-  cin>>perp;
-  
-  hypo = sqrt(pow(base, 2) + pow(perp, 2));
-   
-  cout<<"Hypotenous = "<<hypo; 
+  char oper; 
+  double num1, num2, result;
+
+cout<<"************ CALCULATOR ************"<<endl;
+cout<<"Enter Operation (+,-,*,/)"<<endl;
+cin>>oper;
+cout<<"Enter no-1: "<<endl;
+cin>>num1;
+cout<<"Enter no-2"<<endl;
+cin>>num2;
+
+switch (oper)
+{
+case '+':
+  result = num1+num2;
+  cout<<"Sum = "<<result<<endl;
+  break;
+
+case '-':
+  result = num1-num2;
+  cout<<"Subtration = "<<result<<endl;
+  break;
+
+case '*':
+  result = num1*num2;
+  cout<<"Times = "<<result<<endl;
+  break;
+
+case '/':
+  result = num1/num2;
+  cout<<"Division = "<<result<<endl;
+  break;
+
+default: 
+  cout<<"Follow these operations (+,-,*,/)"<<endl; 
+  break;
+}
+  cout<<"************************************";
   return 0;
 };
