4653824b0b0a7f64f82931e3e3f657fec803db8a
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-20 02:44:20 -0700

    arithematic oparator

--- index.cpp
+++ index.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-/* 
-typedef = 
-reserved keyword used to create an addtional name (alias),
-for another data type. Helps with readability and reduces typos.
-Give a new identifier to current datatype.
-Use when there is a clear benefit
-for defining the name of identifier must end with _t
-*/
-
-typedef std::vector<std::pair<std::string, int>> pairlist_t;
-// typedef std::string text;
-typedef double db;
-// Replaced with 'using' (work better w/ templates)
-using text = std::string;
-using num = int; 
 int main()
 {
-  text code = "typedef used to declare custom data types. ";
-  db pi = 3.1416;
-  num age = 20;
-  cout<<pi<<" and "<<code<<age;
+/* 
+  arithmetic operators  = return the result of a specific arithmetic operation(+,-,*,/, %)
+  parenthesis
+  multiplication & division
+  addition & substration
+*/
+int stock = 100;
+
+//  +
+stock = stock + 1;
+stock+=1;
+stock++;
+
+//  -
+stock = stock - 1;
+stock-=1;
+stock--;
+//  *
+stock = stock * 2;
+stock*=2;
+
+//  /
+stock = stock / 20;
+stock/=20;
+
+// modulus: return the remainder
+
+int rem = stock % 20;
+  cout<<"Stock = "<<stock<<endl;
+  cout<<"Remainder = "<<rem;
   return 0;
 };
