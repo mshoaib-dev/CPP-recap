e2721e77a90399f524c9d78e7581dc0e09712d0a
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-21 05:00:22 -0700

    stringMethods

--- index.cpp
+++ index.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
  
-// ternary operator ?: = replacement for if/else statements
-// condition ? expression1 : expression2;
+// string methods, find the methods names by using dot . against identifier of string
+// for more methods: https://cplusplus.com/reference/string/string/
+
 int main()
 {
-  int chromosome = 46;
-  // chromosome == 46 ? cout<<"You're human" : cout<<"You're aren't";
-  cout<<(chromosome == 46 ? "You're  a human" : "You're aren't");
+  string name; 
+  cout<<"Enter you name: "<<endl;
+  getline(cin, name);
+
+  if(name.length() > 3) cout<<"Welcome "<<name<<endl;
+  else if (name.empty()) cout<<"This field can't be empty"<<endl;
+  else cout<<"Enter at least 3 characters";
+
+  cout<<name.append(", How have you been today?")<<endl;
+  cout<<name.at(2)<<endl; // return the character at iven index, starting from 0
+  cout<<name.insert(0, "#")<<endl; 
+  cout<<name.find('l')<<endl; // to filter something
+  cout<<name.erase(0,3)<<endl;
+  name.clear();
+  cout<<"Hi, "<<name; //clear the identifier
+ 
+  
   return 0;
 };
