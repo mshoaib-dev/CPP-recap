44e08e932d9e4fc36e163deb2c0842f8f7ffcb93
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-21 03:42:36 -0700

    ternaryOperator briefed

--- index.cpp
+++ index.cpp
@@ -1,45 +1,12 @@
 #include <iostream>
 using namespace std;
-
+ 
+// ternary operator ?: = replacement for if/else statements
+// condition ? expression1 : expression2;
 int main()
 {
-  char oper; 
-  double num1, num2, result;
-
-cout<<"************ CALCULATOR ************"<<endl;
-cout<<"Enter Operation (+,-,*,/)"<<endl;
-cin>>oper;
-cout<<"Enter no-1: "<<endl;
-cin>>num1;
-cout<<"Enter no-2"<<endl;
-cin>>num2;
-
-switch (oper)
-{
-case '+':
-  result = num1+num2;
-  cout<<"Sum = "<<result<<endl;
-  break;
-
-case '-':
-  result = num1-num2;
-  cout<<"Subtration = "<<result<<endl;
-  break;
-
-case '*':
-  result = num1*num2;
-  cout<<"Times = "<<result<<endl;
-  break;
-
-case '/':
-  result = num1/num2;
-  cout<<"Division = "<<result<<endl;
-  break;
-
-default: 
-  cout<<"Follow these operations (+,-,*,/)"<<endl; 
-  break;
-}
-  cout<<"************************************";
+  int chromosome = 46;
+  // chromosome == 46 ? cout<<"You're human" : cout<<"You're aren't";
+  cout<<(chromosome == 46 ? "You're  a human" : "You're aren't");
   return 0;
 };
