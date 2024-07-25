7acb21ac0b2948dc8cf631c2775a1c25751fb664
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-25 00:35:25 -0700

    passing an array to a functino

--- index.cpp
+++ index.cpp
@@ -5,45 +5,31 @@ using namespace std;
 ARRAY: A data structure that can hold multiple values, values are accessed by an index number, like a variable that can hold multiple values. It can only contain values of the same data type.
 It’s static data strucute, there is to declare the size of array is we have to define an array or assigning the values later in the code. 
 
-Sizeof() Operator 
-Determine the size in bytes of a: 
-> variable > data type
-> class > object, etc.
-No. of elements in an array = sizeof(arrayName) / sizeof(dataType) 
+PASSING AN ARRAY TO A FUNCTION:
+passing an array to a function in given case, there is use of pointer
+the given function points to the address where the array begins not that array itself, 
+passing an array to a function only require array name not the brackets 
+when a function receives an array, it decays to an pointer, function no longer knows what's the size of an array, we could pass the size to iterate the elements of an array
 
-foreach loop: loop that eases the traversal over an iterable data set. 
-using foreach loop, we can go forward, backward and can skip iteration
-it not use the expression part as used in forLoop
-steps for foreach loop:
-> add the data type iterating over, name of the current element,use colon symbol, name of data set/array-name.....in expression part
 */
 
+double getTotal(double charges[], int size);
 int main()
 {
-  int chair = 10; // 4-bytes
-  string table = "Hexagon"; // 32-bytes
-  bool pass = true; // 1-bytes
-  char grade = 'A'; // 1-bytes
-  char grades[] = {'A', 'B', 'C', 'D', 'E'};  // 5-bytes, each character 1 byte
-  string board[] = {"GM", "DCM", "HR", "CEO", "DM"};  // 160-bytes, each string 32 bytes
-
-  cout<< sizeof(board)<<endl;  
-// No. of elements in an array = sizeof(arrayName) / sizeof(dataType) 
-// sizeof(board)/sizeof(string) == _countof(board);
-  cout<< sizeof(board)/sizeof(string)<<" # of elements"<<endl;  
-  cout<< sizeof(board)/sizeof(board[0])<<" # of elements"<<endl;  
-
-  //iterating array usign foreach loop
-  // traditional approach
-  for (int i = 0; i < sizeof(board)/sizeof(string); i++)
-  {
-    cout<<board[i]<<endl;
-  }
-  // using foreach loop
-  for(string board: board)
-  {
-    cout<<board<<endl;
-  }
-
+  double charges[] = {434.45, 534.45, 53.76, 90.5989, 984};
+  int size = _countof(charges);
+  double total = getTotal(charges, size);
+  cout<<"$"<<total<<"\n";
   return 0;
+};
+
+double getTotal(double charges[], int size){
+  
+  double total = 0;
+  for (int i = 0; i < size; i++)
+  {
+    total += charges[i];
+  }
+  
+  return total;
 };
