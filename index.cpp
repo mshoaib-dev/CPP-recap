fc77d2ba7956d4b4a8f3f16cc0096552de7aba70
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-25 00:06:08 -0700

    arrayIntro-sizeOfOperator-forEachLoop

--- index.cpp
+++ index.cpp
@@ -1,86 +1,49 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-char userChoice();
-char computerChoice();
-void showChoice(char choice);
-void chooseWinner(char player, char computer);
+/*
+ARRAY: A data structure that can hold multiple values, values are accessed by an index number, like a variable that can hold multiple values. It can only contain values of the same data type.
+It’s static data strucute, there is to declare the size of array is we have to define an array or assigning the values later in the code. 
+
+Sizeof() Operator 
+Determine the size in bytes of a: 
+> variable > data type
+> class > object, etc.
+No. of elements in an array = sizeof(arrayName) / sizeof(dataType) 
+
+foreach loop: loop that eases the traversal over an iterable data set. 
+using foreach loop, we can go forward, backward and can skip iteration
+it not use the expression part as used in forLoop
+steps for foreach loop:
+> add the data type iterating over, name of the current element,use colon symbol, name of data set/array-name.....in expression part
+*/
+
 int main()
 {
-  char player, computer;
+  int chair = 10; // 4-bytes
+  string table = "Hexagon"; // 32-bytes
+  bool pass = true; // 1-bytes
+  char grade = 'A'; // 1-bytes
+  char grades[] = {'A', 'B', 'C', 'D', 'E'};  // 5-bytes, each character 1 byte
+  string board[] = {"GM", "DCM", "HR", "CEO", "DM"};  // 160-bytes, each string 32 bytes
 
-  player = userChoice();
-  cout<<"You choose: \n";
-  showChoice(player);
-  computer = computerChoice();
-  cout<<"Computer choose: \n";
-  showChoice(computer);
+  cout<< sizeof(board)<<endl;  
+// No. of elements in an array = sizeof(arrayName) / sizeof(dataType) 
+// sizeof(board)/sizeof(string) == _countof(board);
+  cout<< sizeof(board)/sizeof(string)<<" # of elements"<<endl;  
+  cout<< sizeof(board)/sizeof(board[0])<<" # of elements"<<endl;  
 
-  chooseWinner(player, computer);
+  //iterating array usign foreach loop
+  // traditional approach
+  for (int i = 0; i < sizeof(board)/sizeof(string); i++)
+  {
+    cout<<board[i]<<endl;
+  }
+  // using foreach loop
+  for(string board: board)
+  {
+    cout<<board<<endl;
+  }
+
   return 0;
-};
-
-char userChoice(){
-  char choice;
-
-  cout<<"********** Rock - Paper - Scissor Game  ********* \n";
-  do
-  {
-    cout<<"Choose one of the given:  \n";
-    cout<<"************************ \n";
-    cout<<"'r' for Rock \n";
-    cout<<"'p' for Paper \n";
-    cout<<"'s' for Scissor \n";
-    cin>>choice;
-  } while (choice != 'r' && choice != 'p' && choice != 's');
-  
-  return choice;
-};
-char computerChoice(){
-  srand(time(NULL));
-
-  int num = (rand() % 3) + 1;
-
-  switch (num)
-  {
-  case 1: return 'r';
-  case 2: return 'p';
-  case 3: return 's';
-  default: break;
-  }
-  return 0;
-};
-void showChoice(char choice){
-  switch (choice)
-  {
-  case 'r': cout<<">> Rock \n"; 
-            break;
-  case 'p': cout<<">> Paper \n"; 
-            break;
-  case 's': cout<<">> Scissor \n"; 
-            break;
-  default: cout<<"Invalid choice";
-    break;
-  }
-};
-void chooseWinner(char player, char computer){
-
-  switch (player)
-  {
-  case 'r': if (computer  == 'r') cout<<"It's tie! \n";
-            else if (computer  == 'p') cout<<"You lost!\n";
-            else cout<<"You won! \n";
-            break;
-  case 'p': if (computer  == 'r') cout<<"You won! \n";
-            else if (computer  == 'p') cout<<"It's tie!\n";
-            else cout<<"You lost! \n";
-            break;
-  case 's': if (computer  == 'r') cout<<"You lost! \n";
-            else if (computer  == 'p') cout<<"You won!\n";
-            else cout<<"It's tie\n";
-            break;
-  default:
-    break;
-  }
 };
