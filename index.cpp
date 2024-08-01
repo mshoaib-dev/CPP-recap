e03c61edac0b36ed6466420dc1040afe326180f8
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-01 05:55:11 -0700

    arrayFilling via userInput, StaticApproach

--- index.cpp
+++ index.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 using namespace std;
 
-// fill(): Fills a range of elements with a specified value, 
-// fill(begin, end, value), mostly deals with arrays
-// most use case: filling an array with same entereries manually with large indexes
-// can manipulate for custom range and values
-// there would be blank spaces for not defined range
+// filling an array with a user input, STATIC APPROACH. 
 
 int main()
 {
-  // int nums[10]={1,1,1,1,1,1,1,1,1,1};
-  int nums[2];
-  fill(nums, nums+2, 90);
+  // array initialization
+  string staff[5];
+  int size = sizeof(staff)/sizeof(staff[0]); // no. of elements
+  string temp;
 
-  int size = 8;
-  int frac = 8; // 4 for quarter
-  string places[size];
+  //getting user input
+  for (int i = 0; i < size; i++)
+  {
+    cout<<"Enter name(s) of staff or exit #"<<i+1<<": ";
+    getline(cin, temp);
 
-  fill(places, places + (size/frac), "Skardu");
-  fill(places + (size/frac), places + (size/frac) * 2, "Swat");
-  fill(places + (size/frac) * 2, places + (size/frac) * 3, "Nanga Parbat");
-  fill(places + (size/frac) * 3, places + size, "Kashmir");
+    if(temp == "exit"){
+      break;
+    }
+    else{
+      staff[i] = temp;
+    }
+  }
+   
+  // displaying the results
+  cout<<"You entered: \n";
+  for(int i = 0; !staff[i].empty(); i++){
+    cout<<staff[i]<<endl;
+  };
 
-  for(string place:places){
-    cout<<place<<endl;
-  } 
   return 0;
 };
