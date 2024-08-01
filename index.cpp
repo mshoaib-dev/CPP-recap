dc342153c090124481a4c642f0826c8cb9b8f5cb
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-01 11:23:51 -0700

    quizGame by multidimensionArray

--- index.cpp
+++ index.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// QUIZ GAME
+
 // Multi-dimensional Arrays: arrays made up of separate arrays, mostly 2D arrays
 // useful it can represent grid/matrix of data i.e., rows and columns
 // 1st dimension for ROWS, 2nd dimension for COLUMNS 
@@ -10,23 +12,60 @@ using namespace std;
 int main()
 {
   // two dimensional array declaration:
-  string cars[][3] = {
-    {"Mustang", "Escape", "F-150"}, // by FORD
-    {"Corvette", "Equinox", "Silverado"}, // by CHEVROLET
-    {"Challenger", "Durango", "Ram 1500"} // by DODGE
-  };
+  string questions[] = {"1. Select the Car by FORD.", "2. Select the Car by CHEVROLET." , "3. Select the Car by DODGE." };
 
+  //options
+  string cars[][3] = {
+    {"A. Mustang", "B. Corvette", "C. Challenger"},
+    {"A. F-150", "B. Silverado", "C. Ram 1500"},
+    {"A. Escape", "B. Equinox", "C. Durango"}
+  };
+  
+  // answer key
+  char answerKey[] = {'A', 'B','C'};
+  
   //dimensions
-  int rows = sizeof(cars)/sizeof(cars[0]);
-  int columns = sizeof(cars[0])/sizeof(cars[0][0]);
+  int sizeQues = sizeof(questions)/sizeof(questions[0]);
+  int sizeCars = sizeof(cars[0])/sizeof(cars[0][0]);
   
-  //displaying result
-  for (int i = 0; i < rows; i++)
+  //scoring parameters
+  char guess;
+  int score = 0;
+
+  //answer selection process  
+  cout<<"****************************************** \n";
+  cout<<"***********    QUIZE GAME   ************** \n";
+  cout<<"****************************************** \n";
+
+  for (int i = 0; i < sizeQues; i++)
   {
-    for (int j = 0; j < columns; j++)
-    cout<<cars[i][j]<<", ";
-    cout<<"\n";
+    cout<<questions[i]<<"\n";
+
+    for (int j = 0; j < sizeCars; j++)
+    {
+      cout<<cars[i][j]<<"\n";
+    }
+    
+    //getting user answer
+    cout<<"Choose one of the given options: \n";
+    cin>>guess;
+
+    if (toupper(guess) == answerKey[i])
+    {
+      cout<<"CORRECT! \n";
+      score++;
+    }
+    else
+      cout<<"WRONG! \n";
   }
-  
+
+// displaying result
+  cout<<"****************************************** \n";
+  cout<<"*************    RESULT   **************** \n";
+  cout<<"****************************************** \n";
+  cout<<"You correct answer(s): "<<score<<"\n";
+  cout<<"No. of questions: "<<sizeQues<<"\n";
+  cout<<"Percentage: "<<((double) score/sizeQues)*100<<"%"<<"\n";
+
   return 0;
 };
