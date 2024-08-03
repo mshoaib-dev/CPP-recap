8d330d0587a035a00dbef7482885675c22f1b627
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-03 02:56:49 -0700

    passing variables as a values and reference

--- index.cpp
+++ index.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
 
-// memory address = a location in memory where the data is stored
-// a memory address can accessed with &(ampersand) -> address of Operator
+// passing by values: create copies of parameters, here there would be differenct addresses for each variables, local and being passed. 
+// passing by reference: pass the variables' memory address where the original values are located. 
+
+
+// void swap(string var1, string var2); // passed by values
+void swap(string &var1, string &var2);  // passed by reference
 
 int main()
 {
-  string name = "Shoaib"; // 32 byte
-  bool boolean = true; // 1 byte
-  int num = 65; // 4 bytes  
-
-  //memory address of defined variables
-  cout<<&name<<endl;
-  cout<<&boolean<<endl;
-  cout<<&num<<endl;
-  cout<<sizeof(name);
   
+  string var1 = "Google";
+  string var2 = "Workspace";
+
+  swap(var1, var2);
+
+  cout<<var1<<"\n"<<var2<<endl;
+
   return 0;
+};
+
+// void swap(string var1, string var2){ // passed by values
+void swap(string &var1, string &var2){  // passed by reference
+
+  string temp;
+
+  temp = var1;
+  var1 = var2;
+  var2 = temp;
 };
