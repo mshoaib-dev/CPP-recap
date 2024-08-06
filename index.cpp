524d8076389d3df73954105b3b4e94172458a247
Muhammad Shoaib <122380083+mshoaib-dev@users.noreply.github.com>
2024-08-06 12:40:08 -0700

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define r 3
 #define c 3
 
-// rows and columns elements addition, transport of a matrix implemented here
+// rows and columns elements addition, transport of a matrix implemented here, this is only TRUE for SQUARE Matrix
 int main()
 {
   int arr[r][c] = {{2,1,1},{2,5,2},{3,3,9}};
