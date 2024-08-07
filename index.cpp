2a1eb79d8f92e394b2cc5f79fd5bcdedb70b4dc8
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-07 05:57:43 -0700

    dynamic order matrix multiplication

--- index.cpp
+++ index.cpp
@@ -1,43 +1,91 @@
 #include <iostream>
 using namespace std;
-#define r 2
-#define c 3
+#define MAX 20
 
 int main()
 {
-  int no[r][c] = {
-    {4,5,3},
-    {4,5,3}
-  };
+  int aRow, aCol, bRow, bCol, bMatrix[MAX][MAX], aMatrix[MAX][MAX], productMatrix[MAX][MAX], sum = 0;
+  cout<<"Enter the no. of rows and columns for matrix a: ";
+  cin>>aRow;
+  cin>>aCol;
 
-int sum = 0;
-  // row sum
-  cout<<"Rows sum:    ";
+// input first matrix elements: 
+cout<<"Enter the elements for matrix a: ";
 
-  for (int i = 0; i < r; i++)
+  for (int i = 0; i < aRow; i++)
   {
-    for (int j = 0; j < c; j++)
+    for (int j = 0; j < aCol; j++)
     {
-      sum+=no[i][j];
+      cin>>aMatrix[i][j];
     }
-    cout<<sum<<" ";
-    sum=0;
+  }
+
+//printing a matrix
+cout<<"First matrix: "<<'\n';
+for (int i = 0; i < aRow; i++)
+{
+  for (int j = 0; j < aCol; j++)
+  {
+    cout<<aMatrix[i][j]<<" ";
   }
   cout<<'\n';
+}  
 
-  //column sum
-  cout<<"Columns sum: ";
+// data for second matrix
+cout<<"Enter the no. of rows and columns for matrix b: ";
+cin>>bRow;
+cin>>bCol;
 
-  for (int j = 0; j < c; j++)
+//test the matrix multiplication condition
+if (aCol !=bRow) cout<<"No. of columns of A matrix MUST BE EQUAL to no. of rows of B matrix";
+else
+{
+  cout<<"Enter the elements for matrix b: ";
+
+  for (int i = 0; i < bRow; i++)
   {
-    for (int i = 0; i < r; i++)
+    for (int j = 0; j < bCol; j++)
     {
-      sum+=no[i][j];
+      cin>>bMatrix[i][j];
     }
-    cout<<sum<<" ";
-    sum = 0;
+  }
+    
+//printing b matrix
+cout<<'\n'<<"Second matrix: "<<'\n';
+  for (int i = 0; i < bRow; i++)
+  {
+    for (int j = 0; j < bCol; j++)
+    {
+      cout<<bMatrix[i][j]<<" ";
+    }
+    cout<<'\n';
+  }  
+
+// calculating product of matrix
+  for (int i = 0; i < aRow; i++)
+  {
+    for (int j = 0; j < bCol; j++)
+    {
+      for (int m = 0; m < bRow; m++)
+      {
+        sum += aMatrix[i][m] * bMatrix[m][j];
+      }
+      productMatrix[i][j] = sum;
+      sum = 0;
+    }
+  }
+
+//printing resultant matrix
+cout<<'\n'<<"RESULTANT MATRIX: "<<'\n';
+  for (int i = 0; i < aRow; i++)
+  {
+    for (int j = 0; j < bCol; j++)
+    {
+      cout<<productMatrix[i][j]<<" ";
+    }
+    cout<<'\n';
   }
   
-
+}
   return 0;
 };
