76dfdc8645bcbcf6e43ab23737cf00e6a1dbb231
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-11 22:32:40 -0700

    integer valued dynamicMemory

--- index.cpp
+++ index.cpp
@@ -1,126 +1,29 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-// TIC-TAC-TOE Game
-
-void drawBoard(char *spaces);
-void playerMove(char *spaces, char player);
-void computerMove(char *spaces, char computer);
-bool checkWinner(char *spaces, char player, char computer);
-bool checkTie(char *spaces);
-int playNum = 0;
+/*
+DYNAMIC MEMORY = Memory that is allocated after the program is already complied and running.
+- Use the 'new' operator to allocate memory in the heap rather than the stack.
+- Useful when don't know how much memory we'll need. Make the program more flexible especially when 
+accepting a user input.
+- It's a good practive to delete the dynamic memory literal to reset the memory.
+- 'delete' operator is used alongwith 'new' operator. 
+*/  
 
 int main()
 {
-  char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-  char player = 'X';
-  char computer = 'O';
-  bool running = true; 
-  drawBoard(spaces);
+  //initializing a pointer
+  int *pNum = NULL;
+  pNum = new int; // new keyword followed by data type
+  *pNum = 423;
 
-  while (running)
-  {   
-    playerMove(spaces, player);
-    drawBoard(spaces);
-    if (checkWinner(spaces, player, computer))
-    {
-      running = false;
-      break;
-    }
-    else if(checkTie(spaces))
-    {  
-      running = false;
-      break;
-    }
+  cout<<"Address: "<<pNum<<'\n';
+  cout<<"Value: "<<*pNum<<'\n';
+
+  delete pNum; // deleting heap pointer
   
-    computerMove(spaces, computer);
-    drawBoard(spaces);
-    if (checkWinner(spaces, player, computer))
-    {
-      running = false;
-      break;
-    }
-    else if(checkTie(spaces))
-    {  
-      running = false;
-      break;
-    }
-  }
-  cout<<"Thanks for playing!";
+  cout<<"Updated Address: "<<pNum<<'\n';
+  cout<<"Updated Value: "<<*pNum<<'\n';
+  
   return 0;
 };
-
-void drawBoard(char *spaces){
-  cout<<'\n';
-  cout<<"     |     |     \n";
-  cout<<"  "<<spaces[0]<<"  |  "<<spaces[1]<<"  |  "<<spaces[2]<<"  \n";
-  cout<<"_____|_____|_____\n";
-  cout<<"     |     |     \n";
-  cout<<"  "<<spaces[3]<<"  |  "<<spaces[4]<<"  |  "<<spaces[5]<<"  \n";
-  cout<<"_____|_____|_____\n";
-  cout<<"     |     |     \n";
-  cout<<"  "<<spaces[6]<<"  |  "<<spaces[7]<<"  |  "<<spaces[8]<<"  \n";
-  cout<<"     |     |     \n";
-  cout<<'\n';
-}
-void playerMove(char *spaces, char player){
-   
-  do{
-  cout<<"Enter your move in range (1-9)\n";
-  cin>>playNum;
-  playNum--;
-  if (spaces[playNum] == ' ')
-  {
-    spaces[playNum] = player;
-    break;
-  }
- } while (!playNum > 0 && !playNum < 8);
- 
-}
-void computerMove(char *spaces, char computer){
-  int num;
-  srand(time(0));
-  num = rand() % 9;
-
- do{
-  if (spaces[num] == ' ' && spaces[num] != playNum)
-  {
-    spaces[num] = computer;
-    break;
-  }
- } while (!num > 0 && !num < 8);
-
-}
-bool checkWinner(char *spaces, char player, char computer){
-  
-  if (spaces[0] != ' ' && (spaces[0] == spaces[1] && spaces[1] == spaces[2]))
-  spaces[0] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[3] != ' ' && (spaces[3] == spaces[4] && spaces[4] == spaces[5]))
-  spaces[3] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[6] != ' ' && (spaces[6] == spaces[7] && spaces[7] == spaces[8]))
-  spaces[6] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[0] != ' ' && (spaces[0] == spaces[3] && spaces[3] == spaces[6]))
-  spaces[0] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[1] != ' ' && (spaces[1] == spaces[4] && spaces[4] == spaces[7]))
-  spaces[1] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[2] != ' ' && (spaces[2] == spaces[5] && spaces[5] == spaces[8]))
-  spaces[2] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[0] != ' ' && (spaces[0] == spaces[4] && spaces[4] == spaces[8]))
-  spaces[0] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else if (spaces[2] != ' ' && (spaces[2] == spaces[4] && spaces[4] == spaces[6]))
-  spaces[2] == player ? cout<<"YOU WIN!\n" : cout<<"YOU LOST!\n";
-  else  return false;
-  return true;
-}
-bool checkTie(char *spaces){
-  for (int i = 0; i < 9; i++)
-  {
-    if (spaces[i] == ' ')
-    {
-      return false;
-    }
-  } 
-  cout<<"It's a TIE\n";
-  return true;
-}
