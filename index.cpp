6c24adbb9f41333929c004d5810404aeecd97b5b
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-12 05:40:47 -0700

    struct-array explained

--- index.cpp
+++ index.cpp
@@ -1,36 +1,56 @@
 #include <iostream>
-template <typename C, typename D>
-// using namespace std;
+using namespace std;
 
 /*
-FUNCTION TEMPLATE = describes how the function looks like
-- can be used to generate as many overloaded functions as needed
-- each using different data types
-- overloaded functions (accept and return different data types) based on:
-
-1. The return type of the overloaded function.
-2. The type of the parameters passed to the function.
-3. The number of parameters passed to the function.
-
-- passing different types of parameters for a function can be handled using overloaded functions, but the code 
-becomes messy
-- So, solution is to use TEMPLATES predefined for accepting different types of input having same data type on function invoke
-- The limitation of above can be solved using TEMPLATE PARAMETERS DECLARATION
-- 'auto' keyword will return based on defined template 
-- BENIFIT: just need to write a function once and compatible with a different data types
+STRUCT = a structure that group related variables under one name
+- (array vs struct) > array (multiple values of same data types), struct (multiple values of different data types)
+- once struct is declared by keyword 'struct' followed by identifier name, 
+- Can create multiple variables of that struct like objects of class.
+- name of struct followed by variable name.
+- struct can contain different data types (string, integer, double, char, bool)
+- variable in a struct known as "members"
+- can set default values of members. 
+- memebers can be accessed with a . called as "Class Member Access Operator"
+- passing a struct to a function with their original names will create copies of original, it's what called PASSING BY VALUE
+- PASSING BY REFERENCE will make changes to original rather creating the copies.
+- Can create multiple instances of sturct as need followed by struct identifier.
 */
 
-// given function works for all dataTypes but of same type for inst. invoke
-// C max(C a, C b){
-//   return (a>b) ? a:b;
-// }
+struct Bill
+{
+  int customerId;
+  string consumerName;
+  char type = 'D'; //default set, domestic
+  double billAmount;
+  bool billStatus = true; //default set
+};
 
-// given will return for custom data types, address limitation of above function
-auto max(C a, D b){
-  return (a>b) ? a:b;
-}
+void printBill(Bill &bill);
+void udpateBill(Bill &bill);
 int main()
 {
-  std::cout<<max(90.3333,5.6);
+  //creating sturcts of Bill 
+  Bill bill1;
+  bill1.customerId = 123456;
+  bill1.consumerName = "Consumer";
+  bill1.billAmount = 1234.56;
+  //invoking and function
+  udpateBill(bill1);
+//  cout<<&bill1.billAmount<<'\n';
+  printBill(bill1);
+
+
   return 0;
 };
+
+void udpateBill(Bill &bill){
+  bill.type = 'C'; //commercial
+}
+void printBill(Bill &bill){
+//  cout<<&bill.billAmount<<'\n';
+  cout<<bill.customerId<<'\n';
+  cout<<bill.consumerName<<'\n';
+  cout<<bill.type<<'\n';
+  cout<<bill.billAmount<<'\n';
+  cout<<bill.billStatus<<'\n';
+}
