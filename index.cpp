3e150244bbe94c44c0ee8c0800e476e86bc32d1a
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-12 02:22:34 -0700

    a function addresses different dataTypes

--- index.cpp
+++ index.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
-using namespace std;
+template <typename C, typename D>
+// using namespace std;
 
 /*
-RECURSION = a programming technique where a function invokes itself from within (nested function call)
-- break a complex concept into a repeatable single steps
-- usually use if statement for condition validation and then nested function call
--(iterative vs recursive)
-- ADV. = less code and is cleaner, useful for sorting and searching algorithms
-- DIS. = uses more memory and slower
-- Invokig a function add a frame to a stack, when stack is overloaded, it leads to STACK OVERFLOW
+FUNCTION TEMPLATE = describes how the function looks like
+- can be used to generate as many overloaded functions as needed
+- each using different data types
+- overloaded functions (accept and return different data types) based on:
+
+1. The return type of the overloaded function.
+2. The type of the parameters passed to the function.
+3. The number of parameters passed to the function.
+
+- passing different types of parameters for a function can be handled using overloaded functions, but the code 
+becomes messy
+- So, solution is to use TEMPLATES predefined for accepting different types of input having same data type on function invoke
+- The limitation of above can be solved using TEMPLATE PARAMETERS DECLARATION
+- 'auto' keyword will return based on defined template 
+- BENIFIT: just need to write a function once and compatible with a different data types
 */
 
-int factorial(int n);
+// given function works for all dataTypes but of same type for inst. invoke
+// C max(C a, C b){
+//   return (a>b) ? a:b;
+// }
+
+// given will return for custom data types, address limitation of above function
+auto max(C a, D b){
+  return (a>b) ? a:b;
+}
 int main()
 {
-  int n;
-  cout<<"Enter a # to find factorial: ";
-  cin>>n;
-  cout<<"Factorial = "<<factorial(n);
+  std::cout<<max(90.3333,5.6);
   return 0;
 };
-// iterative approach
-int factorial(int n){
-  int fact = 1;
-  for (n; n > 0; n--)
-  {
-    fact*=n;
-  }
-  return fact;
-}
-
-//recursive approach
-int factorial(int n){
-  if (n > 1)  return n * factorial(n-1); // invoking a function from within 
-  else return 1;
-}
