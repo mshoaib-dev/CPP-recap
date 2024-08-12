0c00faedc0e9a0fd68f9f1b9f40f941f23970629
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-12 13:26:49 -0700

    object-class briefed

--- index.cpp
+++ index.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
 using namespace std;
 
-// defined enum
-enum Mobile {samsung, iPhone, motorolla};
-
 /*
-ENUMS (enumerations) = a user-defined data types that consists of paired named-integer constant. 
-- Great, if you have a set of potential options.
-- take only one value of defined set
-- working with enums and creating a set of data will automatically assign indexes to the elements in a set.
+OBJECT = A collection of attributes (properties) and method (functions)
+- They can have characteristics and could perform actions
+- They can be used to mimic real-world items i.e., Phone, Book, Plantes
+- Relate the properties (attributes) and actions (methods) of real-world items.
+- Created from a class which acts like a "blue-print"
 */
 
+// initializing a class
+class Car
+{
+  public:
+// properties/attributes
+    string make;
+    string model;
+    int year;
+    string color;
+
+//methods (actions by Car)
+  void accelarator(){
+    cout<<"   It accelarates\n";
+  }
+  void ignition(){
+    cout<<"   Self ignition\n";
+  }
+
+};
 
 int main()
 {
-  // int size = size(Mobile);
-  Mobile mobile = samsung;
-  switch (mobile)
-  {
-  case 0: cout<<"It's a Samsung";
-          break;
-  case 1: cout<<"It's a I-Phone";
-          break;
-  case 2: cout<<"It's a Motorolla";
-          break;
-  default:
-    break;
-  }
+//creating instances of class = OBJECT
+  Car car1;
   
+// assining values to properties  
+  car1.make = "Ford";
+  car1.model = "Mustang";
+  car1.year = 2024;
+  car1.color = "Black";
+
+//printing the results
+  cout<<"Brand: "<<car1.make<<'\n';
+  cout<<"Model: "<<car1.model<<'\n';
+  cout<<"Year: "<<car1.year<<'\n';
+  cout<<"Color: "<<car1.color<<'\n';
+
+//invoking methods (actions)
+cout<<"Actions: \n";
+car1.accelarator();
+car1.ignition();
+
   return 0;
 };
