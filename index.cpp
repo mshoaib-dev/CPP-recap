f341cebddacf1b95639e79cb8db78c1e900bcf55
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-29 17:53:38 +0500

    RT-Polymorphism(Function-overriding & Virtual-function)

--- index.cpp
+++ index.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
 using namespace std;
 
-//Static/Compile Time Polymorphism
-//e.g, constructor overloading & function overloading
+//Dynamic/Run Time Polymorphism
+//e.g, 1. Function Overriding, 2. Virtual Functions
+// here Student class's getInfo function will be override by child class GradStudent if child's object is initiated.
 
 class Student{
 public:
   string name; 
-
-//non-parameterized constructor
-  Student(){
-    cout<<"Non-parameterized constructor\n";
-  };
-  
-//parameterized constructor
-  Student(string name){
-    this->name = name;
-    cout<<"Parameterized constructor\n";
-  }
-
-  void getInfo(){
-    cout<<"Non-Parameterized function\n";
-  }
-
+  int rollNo = 123;
+//function overloading-1
   void getInfo(string name){
     this->name = name;
-    cout<<"Name: "<<name;
+    cout<<"Student (Parent Class) = "<<name<<endl;
+  }
+
+//virtual function declared
+  virtual int roll(){
+    cout<<"Roll no from Base Class: ";
+    return rollNo;
+  }
+};
+
+// Derived class
+class GradStudent: public Student{
+public:
+  bool graduated;
+
+//function overloading-2
+  void getInfo(string name){
+  this->name = name;
+  this->graduated = true;
+  cout<<"GradStudent (Child Class)  = "<<name;
+  cout<<endl<<"Status of graduation (Child Class) = "<<graduated<<endl;
+  }
+
+//virtual function overriding
+  int roll(){
+    rollNo = 321;
+    cout<<"Roll no. from Derived Class: ";
+    return rollNo;
   }
 };
 
 int main()
 {
-  Student s1;
-  // Student s1("Muhammad Shoaib");
-  s1.getInfo();
+  GradStudent s1;
   s1.getInfo("Muhammad Shoaib");
-
+  cout<<s1.roll();
   return 0;
 };
