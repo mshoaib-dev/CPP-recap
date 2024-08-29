7a55c9bb9ab63784803a0682b3ef76e1fea1f9cb
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-29 16:32:55 +0500

    CT-Polymorphism(Constructor/Function overloadin)

--- index.cpp
+++ index.cpp
@@ -1,73 +1,39 @@
 #include <iostream>
 using namespace std;
 
-//INHERITANCE (Hierarchical + Single Level Type) = HYBRID INHERITANCE (any 2 types)
+//Static/Compile Time Polymorphism
+//e.g, constructor overloading & function overloading
 
-class Person{  //Base Class
+class Student{
 public:
   string name; 
-  int age;
 
-// custom-constructor
-  Person(string name, int age){
-    this->name = name;
-    this->age = age;
-  }
-
-};
-
-class Student : public Person{ // Derived - 1 from Base Class
-public: int rollNo;
-
-//derived class constructor
-  Student(string name, int age, int rollNo) : Person(name, age){
-    this->rollNo = rollNo;
-  }
-
-//result
-  void getInfo(){
-  cout<<"Name: "<<name<<endl;
-  cout<<"Age: "<<age<<endl;
-  cout<<"RollNo: "<<rollNo<<endl;
-};
-};
-
-class GraduateStudent : public Person{ // Derived - 2 from Base Class 
-public: bool graduate; 
-  GraduateStudent(string name, int age, bool graduate) : Person(name, age){
-    this->graduate = graduate;
+//non-parameterized constructor
+  Student(){
+    cout<<"Non-parameterized constructor\n";
   };
-
-//getting the result  
-  void getInfo(){
-  cout<<"Name: "<<name<<endl;
-  cout<<"Age: "<<age<<endl;
-  cout<<"Graduate: "<<graduate<<endl;
-}
-};
-
-class GTE : public GraduateStudent{ // inherited from GraduateStudent class. (Single Level Inheritance)
-public: string degreeProgram;
-  GTE(string name, int age, bool graduate, string degreeProgram) : GraduateStudent(name, age, graduate){
-    this->degreeProgram = degreeProgram;
+//parameterized constructor
+  Student(string name){
+    this->name = name;
+    cout<<"Parameterized constructor\n";
   }
 
-//getting the result  
   void getInfo(){
-  cout<<"Name: "<<name<<endl;
-  cout<<"Age: "<<age<<endl;
-  cout<<"Graduate: "<<graduate<<endl;
-  cout<<"Degree Program: "<<degreeProgram<<endl;
-}
+    cout<<"Non-Parameterized function\n";
+  }
+
+  void getInfo(string name){
+    this->name = name;
+    cout<<"Name: "<<name;
+  }
 };
 
 int main()
 {
-  GraduateStudent gradStudent1("M.Shoaib", 20, true);
-  gradStudent1.getInfo();
-  Student student1("M.Shoaib", 20, 123);
-  student1.getInfo();
-  GTE gte1("M.Shoaib", 20, true, "Electrical Engineering");
-  gte1.getInfo();
+  Student s1;
+  // Student s1("Muhammad Shoaib");
+  s1.getInfo();
+  s1.getInfo("Muhammad Shoaib");
+
   return 0;
 };
