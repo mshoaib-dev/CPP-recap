2c698306650faede579caf0649f5e04d7cb47c3e
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-29 21:14:34 +0500

    static keyword in function

--- index.cpp
+++ index.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
 
-// ABSTARCTION
-// 1. Access Modifiers/Specifiers, 2. Abstract Class.
-// This code is also implementing runtime polymorphism using virtual function.
+//static keyword
 
-class Shape{ //abstact class
-public: 
-  virtual void draw() = 0; //pure virtual function
+void fun(){
+  // int a=0;
+  static int a=0; // run 1 time on each funI() call.
+  cout<<"a: "<<a<<endl;
+  a++;
+  cout<<"a: "<<a<<endl;
 };
 
-class Square: public Shape{
-public:
-  void draw(){
-    cout<<"Class for drawing Square!";
-  };
-};
 int main()
 {
-  Square s1;
-  s1.draw();
-
+  fun();
+  fun();
+  fun();
   return 0;
 };
