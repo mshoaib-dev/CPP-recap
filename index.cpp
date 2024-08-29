ce3c3ab6930042f882e766dd4f5bb8b6eecfa740
Muhammad Shoaib <mmshooaib@gmail.com>
2024-08-29 18:27:22 +0500

    abstraction via abstract class

--- index.cpp
+++ index.cpp
@@ -1,53 +1,25 @@
 #include <iostream>
 using namespace std;
 
-//Dynamic/Run Time Polymorphism
-//e.g, 1. Function Overriding, 2. Virtual Functions
-// here Student class's getInfo function will be override by child class GradStudent if child's object is initiated.
+// ABSTARCTION
+// 1. Access Modifiers/Specifiers, 2. Abstract Class.
+// This code is also implementing runtime polymorphism using virtual function.
 
-class Student{
-public:
-  string name; 
-  int rollNo = 123;
-  
-//function overloading-1
-  void getInfo(string name){
-    this->name = name;
-    cout<<"Student (Parent Class) = "<<name<<endl;
-  }
-
-//virtual function declared
-  virtual int roll(){
-    cout<<"Roll no from Base Class: ";
-    return rollNo;
-  }
+class Shape{ //abstact class
+public: 
+  virtual void draw() = 0; //pure virtual function
 };
 
-// Derived class
-class GradStudent: public Student{
+class Square: public Shape{
 public:
-  bool graduated;
-
-//function overloading-2
-  void getInfo(string name){
-  this->name = name;
-  this->graduated = true;
-  cout<<"GradStudent (Child Class)  = "<<name;
-  cout<<endl<<"Status of graduation (Child Class) = "<<graduated<<endl;
-  }
-
-//virtual function overriding
-  int roll(){
-    rollNo = 321;
-    cout<<"Roll no. from Derived Class: ";
-    return rollNo;
-  }
+  void draw(){
+    cout<<"Class for drawing Square!";
+  };
 };
-
 int main()
 {
-  GradStudent s1;
-  s1.getInfo("Muhammad Shoaib");
-  cout<<s1.roll();
+  Square s1;
+  s1.draw();
+
   return 0;
 };
