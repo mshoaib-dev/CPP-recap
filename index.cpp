f2f94ad3d4405dd5f322fe7872ec35f76ee92fb0
Muhammad Shoaib <122380083+mshoaib-dev@users.noreply.github.com>
2025-01-12 12:46:17 +0500

    Update index.cpp
    array name updated and index incremented by 1

--- index.cpp
+++ index.cpp
@@ -7,12 +7,12 @@ int searchArray(string array[], int size, string uni);
 int main()
 {
   // top engineering universities in Pakistan
-  string provinces[] = {"GIKI", "PIASS", "NUST", "LUMS", "PU"}, uni;
-  int size = _countof(provinces), rank;
+  string universities[] = {"GIKI", "PIASS", "NUST", "LUMS", "PU"}, uni;
+  int size = _countof(universities), rank;
   
   cout<<"Enter the name of university to search for latest rank: \n";
   getline(cin, uni);
-  rank = searchArray(provinces, size, uni);
+  rank = searchArray(universities, size, uni);
 
   if (rank != -1) cout<<"You entered univerisity rank is: "<<rank<<endl;
   else cout<<"Entered univerity NOT in lasest rank! \n";
@@ -24,7 +24,7 @@ int searchArray(string array[], int size, string uni){
 
   for (int i = 0; i < size; i++){
     if (array[i] == uni) {
-      return i;
+      return i+1;
     }
   }
   
